62fcf7c3372d8126ba3277888659fc7890ea41ec
Trinh Nguyen <trinh.d.nguyen@sjsu.edu>
2016-09-15 21:19:56 -0700

    overwrite file fix

--- src/ViewMainScreenJPanel.java
+++ src/ViewMainScreenJPanel.java
@@ -159,23 +159,25 @@ public class ViewMainScreenJPanel extends JPanel{
 			private static final long serialVersionUID = 1L;
 
 			@Override
-			public void approveSelection(){
+			public void approveSelection() {
 				File f = getSelectedFile();
-				if (f.equals(_mainController.getFile())) {
-					if(getDialogType() == SAVE_DIALOG){
-						int result = JOptionPane.showConfirmDialog(this,"Overwrite file?","Existing file",JOptionPane.YES_NO_CANCEL_OPTION);
-						switch(result){
-						case JOptionPane.YES_OPTION:
-							super.approveSelection();
-							return;
-						case JOptionPane.NO_OPTION:
-							return;
-						case JOptionPane.CLOSED_OPTION:
-							return;
-						case JOptionPane.CANCEL_OPTION:
-							cancelSelection();
-							return;
-						}
+				String filePath = f.getAbsolutePath();
+				if (!filePath.endsWith(".fcq")) {
+					f = new File(filePath + ".fcq");
+				}
+				if (f.exists() && getDialogType() == SAVE_DIALOG) {
+					int result = JOptionPane.showConfirmDialog(this,"Overwrite file?","Existing file",JOptionPane.YES_NO_CANCEL_OPTION);
+					switch(result){
+					case JOptionPane.YES_OPTION:
+						super.approveSelection();
+						return;
+					case JOptionPane.NO_OPTION:
+						return;
+					case JOptionPane.CLOSED_OPTION:
+						return;
+					case JOptionPane.CANCEL_OPTION:
+						cancelSelection();
+						return;
 					}
 				}
 				super.approveSelection();
